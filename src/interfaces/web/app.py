12d44d11299d572d9e25b6d44f24526c240369c9
Edgar Herrador <edgar.herrador@gmail.com>
2025-03-06 19:07:19 -0600

    se agrego  el directorio raíz del proyecto al path de python + se creo el archivo es-MX.json para la trraducción al español

--- src/interfaces/web/app.py
+++ src/interfaces/web/app.py
@@ -1,3 +1,17 @@
+import sys
+import os
+
+# Con la linea sys.path.append(...) estamos agregando el directorio raíz del proyecto al path de python
+# para que pueda importar los módulos que necesite de nuestro proyecto.
+# Ejemplo: si tu archivo app.py está en src/interfaces/web/app.py, entonces el  path del proyecto sería src/
+# por lo que deberías agregar src/ al path de python.
+# Si tu proyecto tiene una estructura diferente, asegurate de agregar el path correcto
+# para que python pueda importar los módulos de tu proyecto sin problemas.
+# Ejemplo:
+# from interfaces.web import algun_modulo  # debe importar el modulo sin ningun problema
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../..")))
+
+# En este caso, estamos importando el módulo chainlit
 import chainlit as cl
 
 @cl.on_message
